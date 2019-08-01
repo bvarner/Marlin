d38b40d7f46bcf794021802a7622b9d991906b98
bvarner <bryan@varnernet.com>
2019-08-01 18:15:58 -0400

    * Preheat with fan for ABS/PETG. * Unload Faster.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1309,7 +1309,7 @@
 
 #define PREHEAT_2_TEMP_HOTEND 240
 #define PREHEAT_2_TEMP_BED    90
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_2_FAN_SPEED   100 // Value from 0 to 255
 
 /**
  * Nozzle Park
